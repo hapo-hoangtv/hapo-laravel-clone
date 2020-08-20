27322f30c017c9ba13c212561599dd97775c47e6
VietHoang <hoang.tv@haposoft.com>
2020-08-20 20:30:09 +0700

    mix laravel

---
+++ resources/js/main.js
@@ -0,0 +1,49 @@
+require('./bootstrap');
+$(document).ready(function(){
+    $('#header-button').click(function() {
+        $('#icon').toggleClass('fa-times');
+        $('#icon').toggleClass('fa-bars');
+    });
+
+    $('.feedback-detail').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      adaptiveHeight: false,
+      autoplay: true,
+      autoplaySpeed : 3000,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      prevArrow: $('.next-left'),
+      nextArrow: $('.next-right'),
+    
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+})
+
+function closeMes(id) {
+  var closeBoxchat = document.getElementById(id);
+  closeBoxchat.style.display = "none";
+}
+
+function showMes(id) {
+  var boxchat = document.getElementById(id);
+  if(boxchat.style.display == "block") {
+      boxchat.style.display = "none";
+  } else {
+      boxchat.style.display = "block";
+  }
+}
+
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/main.js', 'public/js')
+   .js('resources/js/jquery.min.js', 'public/js')
+    .sass('resources/sass/style.scss', 'public/css')
+    .copy('resources/font', 'public/font')
+    .copy('node_modules/slick-slider/slick/slick.min.js', 'public/js')
+    .copy('node_modules/slick-slider/slick/slick.css', 'public/css')
+    .copy('node_modules/slick-slider/slick/slick-theme.css', 'public/css')
+    .copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/fontawesome')
+    .copy('node_modules/font-awesome/css/font-awesome.css', 'public/fontawesome')
+    .options({
+        processCssUrls: false
+    });
